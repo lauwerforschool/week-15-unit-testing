ebd3e03cf3c3ad504da8e04f9117fcb6fb682f10
lauwerforschool <75000815+lauwerforschool@users.noreply.github.com>
2023-04-30 20:49:00 -0500

    fg

---
+++ Group_4_DBTests/JWAuthenticationManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group_4_DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group_4_DB.Controllers;
+
+namespace Group_4_DB.Tests
+{
+    [TestClass()]
+    public class JWAuthenticationManagerTests
+    {
+        [TestMethod()]
+        public void AuthenticateTest()
+        {
+             JWAuthenticationManager manager = new JWAuthenticationManager("fakaelogin");
+
+            User user = new User
+            {
+                username = "test",
+                password= "pass"
+            };
+
+            var ret = manager.Authenticate(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+    }
+}
